42601f1e4736a17c09af4a63b347e93800895c27
Siva Bijinepally <sivakrishna.bijinepally@bbva.com>
2019-06-30 16:57:07 -0500

    Initial Commit

---
+++ telugukathalu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  telugukathalu
+//
+//  Created by Siva Binjinepally on 6/30/19.
+//  Copyright © 2019 Siva Binjinepally. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
